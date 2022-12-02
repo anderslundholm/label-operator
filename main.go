ef006bda86566fa85bb45bdfb46ed782029701ae
Anders Lundholm <anders.lundholm@king.com>
2022-12-02 11:58:33 +0100

    Set up pod label reconcile logic.

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ import (
 
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
+
+	"github.com/anderslundholm/label-operator/controllers"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -30,8 +32,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	"github.com/busser/label-operator/controllers"
+	// "github.com/anderslundholm/label-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
